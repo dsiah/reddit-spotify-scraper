0093b36e77892aff630eb16d54f99845bf2479fb
David Siah <dsiah@villanova.edu>
2014-12-14 13:30:01 -0500

    added color to links

--- public/script.js
+++ public/script.js
@@ -8,7 +8,6 @@
 		}
 		return hashParams;
     }
-    var globalOb = {};
 
     var userProfileSource = 
         document.getElementById('user-profile-template').innerHTML;
@@ -25,7 +24,8 @@
       alert('There was an error during the authentication');
     } else {
         if (access_token) {
-        // render oauth info
+        // once the access token is recieved we need to get some information about
+        // the user -- we can do this by querying the me endpoint 
             $.ajax({
                 url: 'https://api.spotify.com/v1/me',
                 headers: {
@@ -36,7 +36,7 @@
 
                   $('#login').hide();
                   $('#loggedin').show();
-                  globalOb.idme = $('.idme').text();
+                  $('#create').hide();
 
                   $.reddit();
                 }
@@ -47,7 +47,6 @@
           $('#loggedin').hide();
         }
     }
-
     
     $.search = function () {
         $.songs = {};
@@ -64,21 +63,22 @@
             },
             data: '{ "name": "r/Music ' + date + '" }'
         }).done(function (res) {
-            console.log('success: ', res.id);
             $.playlist_id = res.id;
+            $('#search').hide();
+            $('#create').show();
         });
         
 
         $('li > a').each(function (index, item) { 
             var pattern = item.text;
             var reggie = /^[^\[\(]+/;
-
+            var kid = $(this);
             // pattern is processed using regex pattern
             var piece = reggie.exec(pattern)[0];
 
             piece = piece.replace(/-/g, '');
-            piece =  piece.replace(/  /g, '+');
-            piece =  piece.replace(/ /g, '+');
+            piece = piece.replace(/  /g, '+');
+            piece = piece.replace(/ /g, '+');
 
             $.ajax({
                 url: 'https://api.spotify.com/v1/search?q=' 
@@ -89,16 +89,18 @@
                 }
             }).done(function (res) {
                 var unit = res.tracks.items[0];
-                
                 $.songs[piece] = unit;
-                
-                $('.preview').append("<div><a href='" + unit.preview_url + "'>" 
-                    + unit.name + "</a></div>"); 
+
+                if (unit)
+                    kid.addClass("accepted");
             });
         });
     }
 
     $.reddit = function (query) {
+        // Due to XSS (Cross Site Scripting) exploit we must hand the web-scraping 
+        // action to the server, 
+        // once the   
         $.ajax('http://localhost:8888/api/' + query).done(function (data) {
             var ind = 1;
             
@@ -107,9 +109,9 @@
                     + anchor.name + "</a></li>");
                 ind++;
             });
+
             if (data) {
                 $('button.container').hide();
-                console.log("Recieved : ", data); 
             }
         });
     }
@@ -118,13 +120,19 @@
         var purl = "https://api.spotify.com/v1/users/" + $('.idme').text() 
                     + "/playlists/" + $.playlist_id + "/tracks?uris=";  
         var count = 0;
+
+        // create one gigantic url that has all the song uris since firing a request
+        // for each song was not stable :P
         for (var song in $.songs) {
             if (!$.songs[song])
                 continue;
 
+            // encode the songuris ** encodeURI was not working so do this manually
             var songuri = $.songs[song].uri;
             songuri = songuri.replace(/:/g, '%3A');
 
+            // spotify takes songs in this format:
+            // POST /tracks?uris=spotify%3Atracks%3Aaldkfj,spotify%3Atracks%3Aetcetc
             if (count > 0)
                 purl += ',' + songuri;
             else 
@@ -140,7 +148,7 @@
                 'Authorization': 'Bearer ' + access_token
             }
         }).done(function (res) {
-            console.log(res);
+            $('#create').hide();
         });
     }
 })();
